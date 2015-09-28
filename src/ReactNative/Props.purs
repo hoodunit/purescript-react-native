a2c2840a245e29e1fa29fee7753e927f7898709e
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2015-09-28 21:53:20 +0300

    Add onPress prop

--- src/ReactNative/Props.purs
+++ src/ReactNative/Props.purs
@@ -3,13 +3,14 @@ module ReactNative.Props where
 import Prelude
 import Control.Monad.Eff (Eff())
 import Data.Function (mkFn0, mkFn3, mkFn4)
-import React (ReactElement())
+import React (ReactElement(), Event(), EventHandlerContext(), handle)
 import React.DOM.Props (Props(), unsafeMkProps)
 import ReactNative.Components (ListViewDataSource())
 
 type RenderRowFn = forall rowData highlightFn. rowData -> String -> String -> highlightFn -> ReactElement
 type RenderSeparatorFn = String -> String -> Boolean -> ReactElement
 type RenderHeaderFn = forall eff. Eff eff ReactElement
+type OnPressFn = forall eff. Eff eff Unit
 
 foreign import renderHeaderFn :: ReactElement -> (Unit -> ReactElement)
 
@@ -24,3 +25,6 @@ renderHeader elem = unsafeMkProps "renderHeader" (renderHeaderFn elem)
 
 dataSource :: ListViewDataSource -> Props
 dataSource = unsafeMkProps "dataSource" 
+
+onPress :: forall eff props state result. (Event -> EventHandlerContext eff props state result) -> Props
+onPress f = unsafeMkProps "onPress" (handle f)
