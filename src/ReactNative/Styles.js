130d26ff00d6856a7cbc89225dac4eef35b3063f
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2015-10-26 22:24:16 +0200

    Remove unneed console logs

--- src/ReactNative/Styles.js
+++ src/ReactNative/Styles.js
@@ -3,7 +3,6 @@
 // module ReactNative.Styles
 
 exports.createStyleSheet = function(styles){
-  console.log('stylesArray:', styles);
   var stylesObj = {};
   styles.forEach(function(s) { 
     var elemStyles = s.value1;
@@ -11,7 +10,6 @@ exports.createStyleSheet = function(styles){
     elemStyles.forEach(function(e){ elemObj[e[0]] = e[1]; });
     stylesObj[s.value0] = elemObj;
   });
-  console.log('stylesObj:', stylesObj);
   return require('react-native').StyleSheet.create(stylesObj);
 }
 
